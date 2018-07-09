09eb1086335c836315d3a7077805d8fb5d41125f
张晓飞 <876910486@qq.com>
2018-07-09 17:07:20 +0800

    合并 footer到 sina_design

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-
+var moment = require('moment');
 module.exports = {
-    hooks: {
-        config: function(config) {
-            config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
+	hooks: {
+		config: function(config) {
+			config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
 
-            return config;
-        }
-    }
+			return config;
+		},
+		'page:before': function(page) {
+
+			console.log("*************************************");
+			console.log("*************************************");
+			console.log("*************************************");
+			console.log("*************************************");
+
+			console.log(this.options);
+
+			console.log("*************************************");
+			console.log("*************************************");
+			console.log("*************************************");
+
+			var  _copy = 'powered by Gitbook'
+			if(this.options.pluginsConfig['sina_disign']) {
+			   _copy = this.options.pluginsConfig['sina_disign']['copyright'];
+			}
+			var _copyInner = '<span class="copyright">' + _copy + '</span>';
+			
+			var str = ' \n<footer class="page-footer">' + _copyInner + '</footer>';
+			
+			page.content  +=  str;
+			return page;
+
+		}
+	},
+	filters: {
+		date: function(d, format) {
+			return moment(d).format(format)
+		}
+	}
 };
-
-
