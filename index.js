c69e5b2c7cff77ad85a9c79c90f7ad09c6411251
张晓飞 <876910486@qq.com>
2018-07-10 20:55:31 +0800

    添加 干预js的能力

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
 module.exports = {
     hooks: {
+    	
+		init:function(){
+			console.log('******* SINA INIT ***************');
+		},		
         config: function(config) {
             config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
-
             return config;
         }
+        
     }
 };
 
