37b10a6cd5598926eaeb2c4e1bb28f4b7a586d1d
张晓飞 <876910486@qq.com>
2018-07-10 11:01:11 +0800

    修复 bug

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-var moment = require('moment');
+
 module.exports = {
-	hooks: {
-		config: function(config) {
-			config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
+    hooks: {
+        config: function(config) {
+            config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
 
-			return config;
-		},
-		'page:before': function(page) {
-
-			console.log("*************************************");
-			console.log("# Info: running sina_design .........");
-			console.log("*************************************");
-
-			var  _copy = 'powered by Gitbook'
-			if(this.options.pluginsConfig['sina_disign']) {
-			   _copy = this.options.pluginsConfig['sina_disign']['copyright'];
-			}
-			var _copyInner = '<span class="copyright">' + _copy + '</span>';
-			
-			var str = ' \n<footer class="page-footer">' + _copyInner + '</footer>';
-			
-			page.content  +=  str;
-			return page;
-		}
-	},
-	filters: {
-		date: function(d, format) {
-			return moment(d).format(format)
-		}
-	}
+            return config;
+        }
+    }
 };
+
+
